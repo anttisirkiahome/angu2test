8247cb0ad55142d4bee19fe76c72a4646a31413f
Antti Sirkiä <antti.sirkia@futurice.com>
2016-04-23 16:35:34 +0300

    some stuff

--- src/client/app/+item/components/item.component.ts
+++ src/client/app/+item/components/item.component.ts
@@ -2,17 +2,22 @@ import {Component, Input, OnInit} from 'angular2/core';
 
 @Component({
   selector: 'huuto-item',
-  templateUrl: 'app/+item/components/item.component.html' /*,
-  styleUrls: ['app/+about/components/about.component.css']*/
+  //encapsulation: ViewEncapsulation.Native,
+  templateUrl: 'app/+item/components/item.component.html',
+  styleUrls: ['app/+item/components/item.component.css']
 })
 
 export class ItemComponent implements OnInit {
   @Input() item: any;
+  imageUrl: string;
   constructor() {
     console.log('constructing itemComponent');
   }
 
   ngOnInit() {
     console.log('ngOnInit item, ' , this.item);
+
+    this.imageUrl = this.item.images.length > 0 ?
+      this.item.images[0].links.thumbnail : '';
   }
 }
